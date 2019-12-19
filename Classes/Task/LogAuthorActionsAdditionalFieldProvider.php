3aa94eb546e01abbb8c8a90bca08658d2575e115
snaegle1 <naegle@sub.uni-goettingen.de>
2019-12-19 15:52:53 +0100

    Add scheduler task with author actions

--- Classes/Task/LogAuthorActionsAdditionalFieldProvider.php
+++ Classes/Task/LogAuthorActionsAdditionalFieldProvider.php
@@ -42,5 +42,86 @@ use TYPO3\CMS\Scheduler\Task\Enumeration\Action;
  */
 class LogAuthorActionsAdditionalFieldProvider extends AbstractAdditionalFieldProvider
 {
+    /**
+     * @param array $taskInfo
+     * @param AbstractTask $task
+     * @param SchedulerModuleController $schedulerModule
+     * @return array
+     */
+    public function getAdditionalFields(array &$taskInfo, $task, SchedulerModuleController $schedulerModule)
+    {
+        $currentSchedulerModuleAction = $schedulerModule->getCurrentAction();
 
-}
+        // Initialize extra field value
+        if (empty($taskInfo['email'])) {
+            if ($currentSchedulerModuleAction->equals(Action::ADD)) {
+                // In case of new task and if field is empty, set default email address
+                $taskInfo['email'] = $GLOBALS['BE_USER']->user['email'];
+            } elseif ($currentSchedulerModuleAction->equals(Action::EDIT)) {
+                // In case of edit, and editing a test task, set to internal value if not data was submitted already
+                $taskInfo['email'] = $task->email;
+            } else {
+                // Otherwise set an empty value, as it will not be used anyway
+                $taskInfo['email'] = '';
+            }
+        }
+        // Write the code for the field
+        $fieldID = 'task_email';
+        $fieldCode = '<input type="text" class="form-control" name="tx_scheduler[email]" id="' . $fieldID . '" value="' . htmlspecialchars($taskInfo['email']) . '" size="30">';
+        $additionalFields = [];
+        $additionalFields[$fieldID] = [
+            'code' => $fieldCode,
+            'label' => 'LLL:EXT:scheduler/Resources/Private/Language/locallang.xlf:label.email',
+            'cshKey' => '_MOD_system_txschedulerM1',
+            'cshLabel' => $fieldID
+        ];
+        return $additionalFields;
+
+    }
+
+    protected function getNumberOfDaysAdditionalField(array &$taskInfo, $task, SchedulerModuleController $schedulerModule)
+    {
+        $fieldId = 'tx_oafwmGamification_logCommunityAuthorActions_numberOfDays';
+        if (empty($taskInfo[$fieldId])) {
+            $taskInfo[$fieldId] = $task->numberOfDays ?? 180;
+        }
+        $fieldName = 'tx_oafwmGamification_logCommunityAuthorActions_numberOfDays';
+        $fieldHtml = '<input class="form-control" type="text" ' . 'name="' . $fieldName . '" ' . 'id="' . $fieldId . '" ' . 'value="' . (int)$taskInfo[$fieldId] . '" ' . 'size="4">';
+        $fieldConfiguration = [
+            'code' => $fieldHtml,
+            'label' => 'Use entries older than given number of days',
+            'cshLabel' => $fieldId
+        ];
+        return $fieldConfiguration;
+    }
+
+    public function validateNumberOfDaysAdditionalField(array &$submittedData, SchedulerModuleController $schedulerModule)
+    {
+        $submittedData['email'] = trim($submittedData['email']);
+        if (empty($submittedData['email'])) {
+            $this->addMessage(
+                $GLOBALS['LANG']->sL('LLL:EXT:scheduler/Resources/Private/Language/locallang.xlf:msg.noEmail'),
+                FlashMessage::ERROR
+            );
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+
+    }
+
+
+
+    public function validateAdditionalFields(array &$submittedData, SchedulerModuleController $schedulerModule) {
+        $validData = $this->validateNumberOfDaysAdditionalField($submittedData, $schedulerModule);
+        return $validData;
+    }
+    /**
+     * Save additional field in task
+     */
+    public function saveAdditionalFields(array $submittedData, AbstractTask $task)
+    {
+        $task->email = $submittedData['email'];
+    }
+};
--- Classes/Task/LogAuthorActionsTask.php
+++ Classes/Task/LogAuthorActionsTask.php
@@ -44,4 +44,128 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 class LogAuthorActionsTask extends \TYPO3\CMS\Scheduler\Task\AbstractTask implements LoggerAwareInterface
 {
 
+    use LoggerAwareTrait;
+
+    protected $logger = null;
+
+    protected function getData ()
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('pages');
+        $data = $queryBuilder
+            ->select('tt_address.oafwm_uid', 'tt_address.oafwm_groupname', 'log.tstamp', 'log.userid', 'log.IP', 'log.type', 'log.action', 'log.recpid', 'log.recuid', 'log.tablename', 'log.log_data')
+            ->from('tt_address')
+            ->join(
+                'tt_address',
+                'sys_log',
+                'log',
+                $queryBuilder->expr()->eq('log.userid', $queryBuilder->quoteIdentifier('tt_address.oafwm_uid'))
+            )
+            ->where(
+                $queryBuilder->expr()->notLike('log.details',  $queryBuilder->createNamedParameter('%' . $queryBuilder->escapeLikeWildcards('Exception handler') . '%'))
+            )
+            ->orderBy('log.tstamp', 'DESC')
+            ->execute()
+            ->fetchAll(0);
+        return $data;
+    }
+
+
+    protected function getEmailMessage() {
+        $message = "Timestamp; Datum; Uhrzeit; NutzerID; NutzerGroup; IP; Login/Logout; ArtDerAenderung; GeaenderteSeite; GeaendertesElement; AnzahlGeaenderterZeichen;";
+        $message .= LF;
+        $tstamps = $this->getData();
+
+        foreach ($tstamps as $tkey => $tstamp) {
+            // time and date
+            $message .= $tstamp['tstamp'] . '; ';
+            $time = getdate($tstamp['tstamp']);
+            $message .= $time['mday'] .'.'. $time['mon'] .'.'. $time['year'] . '; ';
+            $message .= $time['hours'] .':'. $time['minutes'] .':'. $time['seconds'] . '; ';
+            // id and group
+            $message .= $tstamp['userid'] . '; ';
+            switch ($tstamp['oafwm_groupname']) {
+                case 'Badges':
+                    $message .= "1" . '; ';
+                    break;
+                case 'Level':
+                    $message .= "2" . '; ';
+                    break;
+                case 'Controlgroup':
+                    $message .= "3" . '; ';
+                    break;
+            }
+            $message .= $tstamp['IP'] . '; ';
+            // login or logout
+            if ($tstamp['type'] == '255') {
+                switch ($tstamp['action']) {
+                    case '1':
+                        $message .= "1" . '; ';
+                        break;
+                    case '2':
+                        $message .= "2" . '; ';
+                        break;
+                    case '3':
+                        $message .= "3" . '; ';
+                        break;
+                }
+            } else {
+                $message .= '0' . '; ';
+            }
+            // type of change
+            if ($tstamp['type'] == 1 && $tstamp['tablename'] == 'tt_content') {
+                $message .= "1" . '; ';
+            } elseif ($tstamp['type'] == 1 && $tstamp['tablename'] == 'pages') {
+                $message .= "2" . '; ';
+            } elseif ($tstamp['type'] == 2) {
+                $message .= "3" . '; ';
+            } elseif ($tstamp['type'] == 3) {
+                $message .= "4" . '; ';
+            } else {
+                $message .= "0" . '; ';
+            }
+            // page of change
+            $message .= $tstamp['recpid'] . '; ';
+            // element of change
+            $message .= $tstamp['recuid'] . '; ';
+            // number of changed letters
+            if ($tstamp['tablename'] == 'tt_content') {
+                $logdata = $tstamp['log_data'];
+                $start = strpos($logdata, ':"')+2;
+                $end = strpos($logdata, '";');
+                $length = $end - $start;
+                $substring = substr($logdata, $start, $length);
+                $message .= strlen($substring) . '; ';
+            } else {
+                $message .= "0" . '; ';
+            }
+            $message .= LF;
+        }
+        return $message;
+    }
+
+    /**
+     * @return bool
+     */
+    public function execute() {
+        $authors = $this->getEmailMessage();
+
+        // Prepare mailer and send the mail
+        try {
+            /** @var \TYPO3\CMS\Core\Mail\MailMessage $mailer */
+            $mail = GeneralUtility::makeInstance(MailMessage::class);
+
+            // Create the message
+            $mail->setSubject('Aktivitaeten der Community Autoren');
+            $mail->setBody($authors);
+            $mail->setFrom(array('sibylle@naegle.info' => 'IPOA'));
+            $mail->setTo($this->email);
+            // $mail->setBcc('naegle@sub.uni-goettingen.de');
+            $mailsSend = $mail->send();
+            $success = $mailsSend > 0;
+        } catch (\Exception $e) {
+            throw new \TYPO3\CMS\Core\Exception($e->getMessage(), 1476048416);
+        }
+        return true;
+    }
 }
