0148c8355fb9162fb26d03e6d707714452df9e0b
Sibylle Nägle <naegle@sub.uni-goettingen.de>
2021-06-17 08:57:00 +0200

    Empty repository

--- Resources/Public/JavaScript/fe.js
+++
@@ -1,6 +0,0 @@
-jQuery(() => {
-
-  jQuery('.excluded-from-fe').on ('mouseover click keypress', () => {
-    jQuery('[contenteditable=\'true\']').removeAttr('contenteditable');
-  });
-});
--- ext_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-    die ('Access denied.');
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cop'] = 'EXT:oafwm_gamification/Configuration/RTE/CoP.yaml';
